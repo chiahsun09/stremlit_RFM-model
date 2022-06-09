a0118da96cffea02781ff5ad9bf258066a21a7cd
chiahsun09 <runranrun0911@gmail.com>
2022-06-09 18:17:33 +0800

    test

--- customer_segmentation_stremlit.py
+++ customer_segmentation_stremlit.py
@@ -61,17 +61,17 @@ st.markdown("""---""")
 
 #如果有上傳檔案，即開始執行報表分析
 #df = pd.read_excel(uploaded_file,skiprows=2)
+@st.cache
+def read_file(uploaded_file):
+    df = pd.read_excel(uploaded_file,skiprows=2)
+    return df
 
-if uploaded_file:
-    st.write(uploaded_file)
-    uploaded_file.seek(0)
-    
-    @st.cache
-    def read_file(uploaded_file):
-        df = pd.read_excel(uploaded_file)
-        return df  
-    
+if uploaded_file is not None:
     df=read_file(uploaded_file)
+    st.write(uploaded_file)
+    #uploaded_file.seek(0)
+    
+    
     st.markdown("""<font size="3">●原始檔檢視</font>""",unsafe_allow_html=True)
     df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
     st.dataframe(df,width=1000,height=300)
